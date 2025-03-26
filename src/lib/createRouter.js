e07316184136c756afd6ca17827ff1afbe06e308
Suin Kim <contact@suinkim.me>
2025-03-26 19:06:02 +0900

    fix: 없는 페이지 핸들링

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -4,7 +4,7 @@ import { NotFountPage } from "../pages";
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getTarget = () => routes[window.location.pathname] ?? NotFountPage();
+  const getTarget = () => routes[window.location.pathname] ?? NotFountPage;
 
   const push = (path) => {
     window.history.pushState({}, "", path);
