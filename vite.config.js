88f7423376d7e841891b5ec7b7c721153e641c95
Suin Kim <contact@suinkim.me>
2025-03-27 03:46:46 +0900

    feat: 베이스 URL 추가

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
   },
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "/",
 });
