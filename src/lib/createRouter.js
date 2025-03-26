1387d3ea1637156fa952b2b6eea7382b3d2b81f3
Suin Kim <contact@suinkim.me>
2025-03-26 12:42:13 +0900

    feat: 주소 변경에 반응하는 라우터 생성

---
+++ src/lib/createRouter.js
@@ -0,0 +1,17 @@
+import { createObserver } from "./createObserver";
+import { NotFountPage } from "../pages";
+
+export const createRouter = (routes) => {
+  const { subscribe, notify } = createObserver();
+
+  const getTarget = () => routes[window.location.pathname] ?? NotFountPage;
+
+  const push = (path) => {
+    window.history.pushState({}, "", path);
+    notify();
+  };
+
+  window.addEventListener("popstate", () => notify());
+
+  return { push, subscribe, getTarget };
+};
