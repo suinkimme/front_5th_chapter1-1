a9334914727039a9a07e1a88878753e64ec30881
Suin Kim <contact@suinkim.me>
2025-03-27 17:51:36 +0900

    fix: 기본 라우터 작동안되서 수정함

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,15 +1,14 @@
 import { createObserver } from "./createObserver";
 import { NotFountPage } from "../pages";
-
-const BASE_URL =
-  process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1" : "";
+import { BASE_URL } from "../config";
 
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
   const getPath = () => window.location.pathname.replace(BASE_URL, "");
 
-  const getTarget = () => routes[window.location.pathname] ?? NotFountPage;
+  const getTarget = () =>
+    routes[window.location.pathname.replace(BASE_URL, "")] ?? NotFountPage;
 
   const push = (path) => {
     window.history.pushState({}, "", `${BASE_URL}${path}`);
